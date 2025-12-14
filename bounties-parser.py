1531922af38f055b257aaa94b38764418cbe9d3e
plowsof <plowsof@protonmail.com>
2025-12-14 14:34:37 +0000

    scan all posts + json dump

--- bounties-parser.py
+++ bounties-parser.py
@@ -1,36 +1,39 @@
 import requests
-import pprint 
+from pprint import pprint
 from monerorpc.authproxy import AuthServiceProxy, JSONRPCException
 import csv 
+from collections import defaultdict
+import json
 
-addr_total = {}
+addr_total = defaultdict(dict)
 
-def get_bounty_address(data):
+def get_bounty_addresses():
     global addr_total, unknown
+    info = requests.get("https://bounties.monero.social/api/v1/posts?view=recent&limit=1").json()
+    max_posts = info[0]['id']
     totals = []
-    for post in data:
-        #print(f"title: {post['title']}\nid: {post['id']}")
-        comments = requests.get(f"https://bounties.monero.social/api/v1/posts/{post['id']}/comments").json()
+    for x in range(1,max_posts+1):
+        print(x)
+        try: # if post is deleted this will error and continue to next
+            post_json = requests.get(f"https://bounties.monero.social/api/v1/posts/{x}").json()
+        except:
+            continue
+        title = post_json["title"]
+        comments = requests.get(f"https://bounties.monero.social/api/v1/posts/{x}/comments").json()
         for comment in comments:
+            donation_address = False
             if comment["user"]["name"] == "Monero Bounties Bot":
-                #print(comment["content"])
                 words = comment["content"].split()
                 for word in words:
                     if len(word) == 95:
                         donation_address = word
-                        #print(f"title: {post['title']}\nid: {post['id']}\naddress: {donation_address}")
-                        try:
-                            if addr_total[donation_address]:   
-                                addr_total[donation_address]["title"] = post["title"]
-                        except:
-                            pass
-                        #check for the spammed addresses / don't break
-                        #break
-
-def get_posts(endpoint):
-    info = requests.get(endpoint)
-    data = info.json()
-    get_bounty_address(data)
+                    # [xxx](monero:84cF4zQbTA1) 
+                    if "monero:" in word: # markdown
+                        donation_address = word.split("monero:")[1].replace(")","")
+                if donation_address:
+                    addr_total[donation_address]["title"] = title
+                    addr_total[donation_address].setdefault("amount", 0)
+                    addr_total[donation_address]["post"] = x
 
 #get transfers / subaddress balances
 rpc_connection = AuthServiceProxy(service_url="http://127.0.0.1:18085/json_rpc")
@@ -39,42 +42,25 @@ info = rpc_connection.get_transfers(params)
 for transfer in info["in"]:
     address = transfer["address"]
     amount = transfer["amount"]
-    data = {
-    "amount": amount,
-    "title": ""
-    }
-    try:
-        if addr_total[address]:
-            addr_total[address]["amount"] += data["amount"]
-    except:
-        addr_total[address] = data
+    addr_total[address].setdefault("title", "")
+    addr_total[address].setdefault("amount", 0)
+    addr_total[address]["amount"] += amount
 
-get_posts("https://bounties.monero.social/api/v1/posts")
-get_posts("https://bounties.monero.social/api/v1/posts?view=completed")
-get_posts("https://bounties.monero.social/api/v1/posts?view=planned")
-get_posts("https://bounties.monero.social/api/v1/posts?view=started")
-get_posts("https://bounties.monero.social/api/v1/posts?view=declined")
+get_bounty_addresses()
 
-
+with open("data.json", "w", encoding="utf-8") as f:
+    json.dump(addr_total, f)
 
 with open("bounty-totals.csv", "w+") as f:
     #f.write("subaddress,amount,title\n")
     for addr in addr_total:
-        if not addr_total[addr]["title"]:
-            print(addr_total[addr]["amount"])
-            print(addr)
         amnt = addr_total[addr]["amount"] * (10 ** -12)
-        print(amnt)
         f.write(f"{addr},{amnt},\"{addr_total[addr]['title']}\"\n")
-
-
 
 with open('bounty-totals.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     sortedlist = sorted(csv_reader, key=lambda row: float(row[1]), reverse=True)
-    pprint.pprint(sortedlist)
-
-
+    pprint(sortedlist)
 
 with open('bounty-totals.csv', "w+") as f:
     f.write("subaddress,amount,title")
